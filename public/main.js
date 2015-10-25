bba8ac1632ab1614a8ff0d9e31d63224f7a3aa86
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2015-10-25 14:52:45 +0330

    some html added

--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
-    var $searchBtn = $("#serachBtn") ;
-    var $searchfiled = $("#searchField") ;
+    var $searchBtn = $("#searchBtn") ;
+    var $searchField = $("#searchField") ;
+    var $searchStatus = $("#searchStatus") ;
+
+    var $userName = $("#userName") ;
+    var $password = $("#password") ;
+    var $loginBtn = $("#loginBtn") ;
+
+    $loginBtn.click(function () {
+        $.post("/login" , {user : $userName.val() , pass : $password.val() } , function (data) {
+            console.log(data) ;
+
+        })  ;
+    }) ;
+    var renderIndexTable = function () {
+
+    } ;
+
+
+    $searchBtn.click(function () {
+        var value = $searchField.val() ;
+        $searchStatus.html("You Searched for : " + value ) ;
+    });
+
+
 
 }) ;
