94691d6f622355d0c5075f2e84939e4756364a12
Arian <arian.ah@hotmail.com>
2015-10-25 09:48:20 +0330

    initialized project & installed dependencies,made a sample

---
+++ index.js
@@ -0,0 +1,47 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var HOST = 'localhost';
+var PORT = 3306;
+var mysql      = require('mysql');
+var connection = mysql.createConnection({host:HOST,port:PORT});
+
+
+var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded());
+
+//owasp #1 sql injection
+app.post('/login', function (req, res) {
+	var user = req.body.user;
+	var pass = req.body.pass;
+  //var user = connection.escape(req.body.user);
+  //var pass = connection.escape(req.body.pass);
+  console.log("user: ",user);
+  console.log("pass: ",pass);
+  var sql  = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'";
+  connection.query(sql, function(err, results) {
+  	console.log(results);
+  });
+
+});
+app.post('/safe/Login',function(req,res) {
+	var user = connection.escape(req.body.user);
+	var pass = connection.escape(req.body.pass);
+	console.log("user: ",user);
+	console.log("pass: ",pass);
+	var sql  = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'";
+	connection.query(sql, function(err, results) {
+		console.log(results);
+	});
+});
+//cmd=while(1){console.log(%22HACKED%22)}
+app.get('/nodejsInjection',function(req,res){
+	console.log(req.query['cmd']);
+	eval(req.query['cmd']);
+});
+app.get('/',function(req,res){
+	console.log('sth else');
+});
+
+app.listen(8080);
